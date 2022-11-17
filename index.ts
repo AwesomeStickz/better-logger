07b34504b704e561b00e7f4cf3c021145a97a8f3
Awesome Stickz <kiran121102@gmail.com>
2022-11-17 18:52:57 +0530

    Log date, time in 2 digits (like 01 instead 1)

--- index.ts
+++ index.ts
@@ -3,9 +3,16 @@ import chalk from 'chalk';
 const { error, info, log, warn } = console;
 
 const getDebugInfo = () => {
-    const date = new Date();
+    const dateObject = new Date();
 
-    return chalk.gray(`[${date.toLocaleDateString()} ${date.toLocaleTimeString()} ${date.getTime()}]`);
+    // Date in the Format: MM/DD/YY
+    const date = dateObject.toLocaleDateString('en-GB', { month: '2-digit', day: '2-digit', year: '2-digit' });
+    // Time in the Format: HH:MM:SS AM/PN
+    const time = dateObject.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    // Unix Timestamp
+    const timestamp = dateObject.getTime();
+
+    return chalk.gray(`[${date} ${time} ${timestamp}]`);
 };
 
 console.error = (...args) => error(getDebugInfo(), chalk.red(...args));
