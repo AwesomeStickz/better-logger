84459e3a1f26fa4a219479abe792ba8a0c57b7ca
Awesome Stickz <kiran121102@gmail.com>
2022-11-19 21:08:16 +0530

    Fix typo

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const getDebugInfo = () => {
 
     // Date in the Format: MM/DD/YY
     const date = dateObject.toLocaleDateString('en-GB', { month: '2-digit', day: '2-digit', year: '2-digit' });
-    // Time in the Format: HH:MM:SS AM/PN
+    // Time in the Format: HH:MM:SS AM/PM
     const time = dateObject.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
     // Unix Timestamp
     const timestamp = dateObject.getTime();
