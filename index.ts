ea22a447458843fad10a5033c5d0cd10025abc14
Awesome Stickz <awesome@stickz.dev>
2024-04-04 23:17:26 +0530

    Show ms too in the logs

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const getDebugInfo = () => {
     // Date in the Format: MM/DD/YY
     const date = dateObject.toLocaleDateString('en-GB', { month: '2-digit', day: '2-digit', year: '2-digit' });
     // Time in the Format: HH:MM:SS AM/PM
-    const time = dateObject.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    const time = dateObject.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', fractionalSecondDigits: 3 });
     // Unix Timestamp
     const timestamp = dateObject.getTime();
 
